904982e03bf9a983da2ce24abca541f355a78a6d
Peter Jemley <peter.jemley@me.com>
2025-12-29 09:56:10 -0800

    Initial Commit

---
+++ ArabicOntology/ArabicOntologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArabicOntologyApp.swift
+//  ArabicOntology
+//
+//  Created by Peter Jemley on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ArabicOntologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArabicOntologyTests/ArabicOntologyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArabicOntologyTests.swift
+//  ArabicOntologyTests
+//
+//  Created by Peter Jemley on 12/29/25.
+//
+
+import Testing
+@testable import ArabicOntology
+
+struct ArabicOntologyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
